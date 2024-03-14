a4325ca73f3a7e24f8955d8c3793b17373f16f32
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-03-14 14:54:18 +0100

    feat: Graphic Interface

---
+++ src/IGraphic.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** IGraphic
+*/
+
+#pragma once
+
+#include "IEntity.hpp"
+#include "IText.hpp"
+#include "ISound.hpp"
+#include <memory>
+
+class IGraphic {
+    public:
+        IGraphic() = default;
+        virtual ~IGraphic() = default;
+
+        //Window
+        virtual bool isWindowOpen() const = 0;
+        virtual void closeWindow() = 0;
+        virtual void clearWindow() = 0;
+
+        //Event
+        virtual int getKeyEvent() = 0;
+
+        //Display
+        virtual void displayWindow() = 0;
+        virtual void displayEntities(std::vector<std::shared_ptr<IEntity>> entities) = 0;
+        virtual void displayText(std::vector<std::shared_ptr<IText>> texts) = 0;
+        virtual void playSound(std::vector<std::shared_ptr<ISound>> sounds) = 0;
+};

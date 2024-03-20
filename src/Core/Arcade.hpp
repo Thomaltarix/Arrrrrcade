d7cb524257d9e267465e3fafdaa33344bca0ba4c
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-03-20 16:41:44 +0100

    feat: create the Arcade class

---
+++ src/Core/Arcade.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** Arcade class
+*/
+
+#pragma once
+
+class Arcade {
+
+    public:
+        Arcade();
+        ~Arcade();
+
+    private:
+};

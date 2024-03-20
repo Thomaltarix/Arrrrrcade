a965ff43ea0449ef7f46f26a3acb212db84c1e42
Théophile JR <theophilejeromerocher44@gmail.com>
2024-03-20 17:23:09 +0100

    feat: handle key events + display entities + display text

--- src/Graphical/NCURSES/ncurses.cpp
+++ src/Graphical/NCURSES/ncurses.cpp
@@ -8,11 +8,12 @@
 #include "ncurses.hpp"
 #include <ncurses.h>
 
-NCURSESlib::NCURSESlib(){
+NCURSESlib::~NCURSESlib(){
     _isOpen = false;
 };
 
-NCURSESlib::~NCURSESlib(){
+NCURSESlib::NCURSESlib(){
+    _isOpen = true;
     initscr();
     keypad(stdscr, TRUE);
     resizeterm(LINES, COLS);
@@ -36,17 +37,34 @@ void NCURSESlib::displayWindow() {
     refresh();
 };
 
-//----
-
 int NCURSESlib::getKeyEvent() {
     int touche = getch();
 
+    if (touche == '\033'){
+        touche = getch();
+    }
+    if (touche == 27)
+        closeWindow();
     return touche;
 };
-void NCURSESlib::displayEntities(std::vector<std::shared_ptr<IEntity>> entities){
-    
+
+void NCURSESlib::displayEntities(std::vector<std::shared_ptr<IEntity>> entities) {
+    int char_todisp;
+
+    for (auto &entity : entities) {
+        char_todisp = entity.get()->getChar();
+        mvprintw(entity.get()->getPos()[0], entity.get()->getPos()[1], reinterpret_cast<char*>(&char_todisp));
+    }
 };
 
 void NCURSESlib::displayText(std::vector<std::shared_ptr<IText>> texts){
-    
+    for (auto &text : texts) {
+        mvprintw(text.get()->getPos()[0], text.get()->getPos()[1], text.get()->getText().c_str());
+    }
+};
+
+void NCURSESlib::playSound(std::vector<std::shared_ptr<ISound>> sounds) {
+    (void) sounds;
+
+    return;
 };
--- src/Graphical/NCURSES/ncurses.hpp
+++ src/Graphical/NCURSES/ncurses.hpp
@@ -16,54 +16,54 @@ class NCURSESlib : public AGraphic {
     private:
         bool _isOpen;
     const std::unordered_map<int, int> _keyMap = {
-        {97, 0},    // A
-        {98, 1},    // B
-        {99, 2},    // C
-        {100, 3},   // D
-        {101, 4},   // E
-        {102, 5},   // F
-        {103, 6},   // G
-        {104, 7},   // H
-        {105, 8},   // I
-        {106, 9},   // J
-        {107, 10},  // K
-        {108, 11},  // L
-        {109, 12},  // M
-        {110, 13},  // N
-        {111, 14},  // O
-        {112, 15},  // P
-        {113, 16},  // Q
-        {114, 17},  // R
-        {115, 18},  // S
-        {116, 19},  // T
-        {117, 20},  // U
-        {118, 21},  // V
-        {119, 22},  // W
-        {120, 23},  // X
-        {121, 24},  // Y
-        {122, 25},  // Z
-        {27, 26},   // Escape
-        {9, -1},    // Tab
-        {16, -1},   // LShift
-        {29, -1},   // LControl
-        {' ', 30},   // Space
-        {KEY_ENTER, 31},   // Enter
-        {KEY_BACKSPACE, 32},  // BackSpace
-        {KEY_UP, 33},  // Up
-        {KEY_DOWN, 34},  // Down
-        {KEY_LEFT, 35},  // Left
-        {KEY_RIGHT, 36},  // Right
-        {48, '0'},   // Num0
-        {49, '1'},   // Num1
-        {50, '2'},   // Num2
-        {51, '3'},   // Num3
-        {52, '4'},   // Num4
-        {53, '5'},   // Num5
-        {54, '6'},   // Num6
-        {55, '7'},   // Num7
-        {56, '8'},   // Num8
-        {57, '9'}    // Num9
-    };
+            {'a', 0},
+            {'b', 1},
+            {'c', 2},
+            {'d', 3},
+            {'e', 4},
+            {'f', 5},
+            {'g', 6},
+            {'h', 7},
+            {'i', 8},
+            {'j', 9},
+            {'k', 10},
+            {'l', 11},
+            {'m', 12},
+            {'n', 13},
+            {'o', 14},
+            {'p', 15},
+            {'q', 16},
+            {'r', 17},
+            {'s', 18},
+            {'t', 19},
+            {'u', 20},
+            {'v', 21},
+            {'w', 22},
+            {'x', 23},
+            {'y', 24},
+            {'z', 25},
+            {27, 26},
+            // {sf::Keyboard::Tab, 27},
+            // {sf::Keyboard::LShift, 28},
+            // {sf::Keyboard::LControl, 29},
+            {' ', 30},
+            {KEY_ENTER, 31},
+            {KEY_BACKSPACE, 32},
+            {KEY_UP, 33},
+            {KEY_DOWN, 34},
+            {KEY_LEFT, 35},
+            {KEY_RIGHT, 36},
+            {'0', 37},
+            {'1', 38},
+            {'2', 39},
+            {'3', 40},
+            {'4', 41},
+            {'5', 42},
+            {'6', 43},
+            {'7', 44},
+            {'8', 45},
+            {'9', 46}
+        };
 
     public:
         NCURSESlib();
@@ -81,4 +81,5 @@ class NCURSESlib : public AGraphic {
         void displayWindow();
         void displayEntities(std::vector<std::shared_ptr<IEntity>> entities) override;
         void displayText(std::vector<std::shared_ptr<IText>> texts) override;
+        void playSound(std::vector<std::shared_ptr<ISound>> sounds) override;
 };

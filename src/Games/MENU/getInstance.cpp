9d75892837e159fff5bda93f2805886f743fb305
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-03-22 11:34:34 +0100

    feat: getInstance file to create the menu instance

---
+++ src/Games/MENU/getInstance.cpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** menu
+*/
+
+#include <iostream>
+#include "menu.hpp"
+#include <memory>
+
+extern "C"
+{
+    __attribute__((constructor))
+    static void initsharedlibrary()
+    {
+        std::cout << "Loading Menu library ..." << std::endl;
+    }
+
+    std::unique_ptr<MENUlib> loadGameInstance()
+    {
+        return std::make_unique<MENUlib>();
+    }
+
+    __attribute__((destructor))
+    static void destroysharedlibrary()
+    {
+        std::cout << "Menu closing ..." << std::endl;
+    }
+}

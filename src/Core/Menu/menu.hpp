adf35792b7376d620c49bc7f9021585d1c9fb3c6
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-03-27 19:00:23 +0100

    feat: commentary in menu hpp

--- src/Core/Menu/menu.hpp
+++ src/Core/Menu/menu.hpp
@@ -23,25 +23,30 @@ namespace Arcade {
                 std::vector<std::string> _listGraphic;
                 std::vector<std::string> _listGame;
 
+                //current libs selected
                 std::string _selectedGraphic;
                 std::string _selectedGame;
 
+                //position cursor
                 std::vector<std::size_t> _cursor;
 
+                //username
                 std::string _userNameDisplay;
                 std::string _userName;
                 std::size_t _userNameIndex;
 
-
+                //create entity
                 void createEntityBackground();
                 void createEntityPlay();
                 void createLibsButtons();
                 void createUserName();
                 void createButton(std::size_t posx, std::size_t posy, std::size_t sizex, std::size_t sizey, std::string path);
 
+                //create texts
                 void createText(std::string text, std::size_t posx, std::size_t posy, std::size_t size);
                 void createLibsTexts();
 
+                //key events
                 void handleArrowKey(int key);
                 void handleEnterKey(int key);
                 void keyUp();
@@ -49,6 +54,7 @@ namespace Arcade {
                 void keyLeft();
                 void keyRight();
 
+                //buttons states
                 void changeButtonState(std::string path);
 
             public:

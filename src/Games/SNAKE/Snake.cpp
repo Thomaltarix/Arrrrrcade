55e90864fbf286ad72f7654e6a8dd511d2791880
Thomaltarix <thomas.boue@epitech.eu>
2024-03-30 17:41:10 +0100

    feat: Add the score display in real-time

--- src/Games/SNAKE/Snake.cpp
+++ src/Games/SNAKE/Snake.cpp
@@ -98,9 +98,9 @@ std::vector<std::shared_ptr<Arcade::IEntity>> Arcade::SnakeGame::getEntities()
 std::vector<std::shared_ptr<Arcade::IText>> Arcade::SnakeGame::getTexts()
 {
     _texts.clear();
-    // std::string score = "Score" + std::to_string(_score);
-    // std::shared_ptr<Arcade::IText> text = std::make_shared<Arcade::Text>(score, 0, 0);
-    // _texts.push_back(text);
+    std::string score = "Score: " + std::to_string(_score);
+    std::shared_ptr<Arcade::IText> text = std::make_shared<Arcade::Text>(score, 9, 5);
+    _texts.push_back(text);
     return _texts;
 }
 
